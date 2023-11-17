dda5086e504a1c9ff7b0a91ac24a6372a4a88807
hubisk <rubenskisonas@gmail.com>
2023-11-16 21:50:18 -0300

    feat:Primeiro contato JavaScript

---
+++ JavaScript/inicio/trabalhando_com_dados/script01.js
@@ -0,0 +1,18 @@
+var fabricanteCarro = "Renault"
+let modeloCarro = "Logan"
+var valorCarro = 16000
+var anoCarro = 2018
+
+// console.log("Carros")
+// console.log(fabricanteCarro)
+// console.log(modeloCarro)
+// console.log(valorCarro)
+// console.log(anoCarro)
+
+// modeloCarro = "Sandero"
+// modeloCarro = "Clio"
+// console.log(modeloCarro)
+
+console.log("Fabricante: " + fabricanteCarro + ", Modelo: " + modeloCarro + ", Valor: R$" + valorCarro + ", Ano: " + anoCarro + ".")
+// Concatenação dos valores das variáveis com textos em Strings
+
