1c8cb11e965bd6f9980e36fad7dd6750d6a02b63
hubisk <rubenskisonas@gmail.com>
2023-11-17 21:41:24 -0300

    feat: Ex JavaScript, aula dia 17/11/23

--- JavaScript/inicio/trabalhando_com_dados/desafio1.js
+++ JavaScript/inicio/trabalhando_com_dados/desafio1.js
@@ -5,5 +5,24 @@ Lembre-se da estrutura dos dados (Entrada de dados, Processamento e Saída de da
 tendo em mente que o raio da circunferência é de R = 16. */
 
 
+// Este é o meu código para o desafio, o seguinte é a forma correta
 
-const = Math.PI
+// Entrada de Dados
+// let raioCirc = 16
+// const pi = Math.PI
+
+// Processamento de Dados
+// const area = (raioCirc * raioCirc) * pi
+
+// Saída de Dados
+// console.log(`A área da circunferência é de ${area} cm².`);
+
+// Entrada de Dados
+let raioCirc = 16
+
+// Processamento de Dados
+const area = (raioCirc * raioCirc) * Math.PI
+
+// Saída de Dados
+console.log(`A área da circunferência é de ${area.toFixed(4)} cm².`);
+// O atributo .toFixed() limita a quantidade de caracteres exibidos no log
